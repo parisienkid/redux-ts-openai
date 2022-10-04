f2a0db4fb1eea563d2e9afc321bd2fd9e2706f7b
Danya Boykov <danyaboickov@icloud.com>
2022-10-04 16:22:55 +0300

    Media for ResearchPage
    Media for R.P + InfoBlock update

--- src/components/info-block/InfoBlock.tsx
+++ src/components/info-block/InfoBlock.tsx
@@ -1,17 +1,19 @@
 import styled from "styled-components"
+import { Link } from "react-router-dom"
 
 export const InfoBlock = styled.div`
     border-top: 1px solid rgba(0,0,0,.1);
     padding: 75px 0 100px 0;
     display: grid;
-    grid-template-columns: 4fr 6fr;
-    grid-column-gap: 150px;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 30px;
     h3 {
         margin-bottom: 30px;
         margin-top: 0;
     }
     p {
         margin-bottom: 30px;
+        max-width: 600px;
     }
     a {
         margin-left: -18px;
@@ -20,7 +22,7 @@ export const InfoBlock = styled.div`
         }
     }
     @media ${({theme}) => theme.media.extraLarge} {
-        grid-column-gap: 100px;
+        grid-column-gap: 30px;
     }
     @media ${({theme}) => theme.media.large} {
         grid-template-columns: 1fr;
@@ -42,4 +44,30 @@ export const InfoDescr = styled.div`
 
 export const InfoMain = styled.div`
     width: 100%;
+`
+
+export const InfoTitleLink = styled(Link)`
+    color: rgb(0,0,0);
+    font-size: .85rem;
+    display: block;
+    font-family: ${({theme}) => theme.fonts.colfax};
+    letter-spacing: .3px;
+    margin-left: 0 !important;
+    &:hover {
+        color: rgba(0,0,0);
+    }
+`
+
+export const InfoSubtitleLink = styled(InfoTitleLink)`
+    color: rgba(0,0,0, .5);
+    margin-top: 2px;
+    &:hover {
+        color: rgba(0,0,0, .5);
+    }
+`
+
+export const InfoLinksWrapper = styled(Link)`
+    margin-bottom: 40px;
+    display: block;
+    margin-left: 0 !important;
 `
--- src/pages/research-page/ResearchPage.tsx
+++ src/pages/research-page/ResearchPage.tsx
@@ -13,16 +13,24 @@ import clipGrad from '../../assets/images/research/clip-gradient.svg';
 import imageGrad from '../../assets/images/research/image-gradient.svg';
 import jukeboxGrad from '../../assets/images/research/jukebox-gradient.svg';
 import solvingGrad from '../../assets/images/research/solving-gradient.svg';
-import { InfoBlock, InfoDescr, InfoMain } from '../../components/info-block/InfoBlock';
+import { InfoBlock, InfoDescr, InfoMain, InfoLinksWrapper } from '../../components/info-block/InfoBlock';
+import { StyledRouterBtn } from '../../components/buttons/Btn';
 
 
 const ResearchPageWrapper = styled.div`
-    padding-top: 70px;
+    padding: 70px 0;
     h2 {
         max-width: 700px;
     }
-    h3 {
-        margin-top: 120px;
+    @media ${({theme}) => theme.media.large} {
+        padding: 45px 0;
+    }
+`
+
+const MaintTitle = styled.h3`
+    margin-top: 120px;
+    @media ${({theme}) => theme.media.medium} {
+        margin-top: 70px;
     }
 `
 
@@ -34,6 +42,9 @@ const MilestonesWrapper = styled.div`
     grid-column-gap: 30px;
     grid-row-gap: 60px;
     margin-bottom: 80px;
+    @media ${({theme}) => theme.media.medium} {
+        grid-template-columns: 1fr;
+    }
 `
 
 const MItem = styled.a`
@@ -70,6 +81,15 @@ const MVideo = styled.video`
     left: 50%;
     transform: translateY(-50%) translateX(-50%);
     box-shadow: 0 0 25px rgb(0 0 0 / 15%);
+    @media ${({theme}) => theme.media.large} {
+        max-width: 65%;
+    }
+    @media ${({theme}) => theme.media.medium} {
+        max-width: 326px;
+    }
+    @media ${({theme}) => theme.media.small} {
+        max-width: 170px;
+    }
 `
 
 const MImg = styled.img`
@@ -80,25 +100,36 @@ const MImg = styled.img`
     left: 50%;
     transform: translateY(-50%) translateX(-50%);
     box-shadow: 0 0 25px rgb(0 0 0 / 15%);
-
+    @media ${({theme}) => theme.media.large} {
+        max-width: 65%;
+    }
+    @media ${({theme}) => theme.media.medium} {
+        max-width: 326px;
+    }
+    @media ${({theme}) => theme.media.small} {
+        max-width: 170px;
+    }
 `
 
 
 const MDescr = styled.h4`
     margin-top: 10px;
     font-size: .8rem;
-    line-height: .8rem;
+    line-height: 1rem;
     color: rgba(0,0,0);
     font-family: ${({theme}) => theme.fonts.colfax};
 `
 
 const MTime = styled.time`
-    margin-top: 10px;
+    display: block;
+    margin-top: 7px;
     font-size: .8rem;
     line-height: .8rem;
     color: rgba(0,0,0,.5);
     font-family: ${({theme}) => theme.fonts.colfax};
 `
+
+
 
 const ResearchPage: FC = () => {
 
@@ -113,7 +144,7 @@ const ResearchPage: FC = () => {
             <ResearchPageWrapper>
                 <Container>
                     <h2>OpenAI conducts fundamental, long-term research toward the creation of safe AGI.</h2>
-                    <h3>Milestones</h3>
+                    <MaintTitle>Milestones</MaintTitle>
                     <MilestonesWrapper>
                         <MItem>
                             <MPicture>
@@ -173,7 +204,19 @@ const ResearchPage: FC = () => {
                             <h3>Blog</h3>
                         </InfoDescr>
                         <InfoMain>
-                            
+                           <InfoLinksWrapper to='/'>
+                                <MDescr >Introducing Whisper</MDescr>
+                                <MTime >September 21, 2022 — Research</MTime>
+                           </InfoLinksWrapper>
+                           <InfoLinksWrapper to='/'>
+                                <MDescr >Our approach to alignment research</MDescr>
+                                <MTime >August 24, 2022 — Research</MTime>
+                           </InfoLinksWrapper>
+                           <InfoLinksWrapper to='/'>
+                                <MDescr >DALL·E 2 Pre-Training Mitigations</MDescr>
+                                <MTime >June 28, 2022 — Research</MTime>
+                           </InfoLinksWrapper>
+                            <StyledRouterBtn $nobg color='0,0,0' to="" after='▻' background=''>VIEW ALL POSTS</StyledRouterBtn>
                         </InfoMain>
                     </InfoBlock>
                     <InfoBlock>
@@ -181,7 +224,19 @@ const ResearchPage: FC = () => {
                             <h3>Publications</h3>
                         </InfoDescr>
                         <InfoMain>
-                            
+                            <InfoLinksWrapper to='/'>
+                                <MDescr >Efficient Training of Language Models to Fill in the Middle</MDescr>
+                                <MTime >July 28, 2022</MTime>
+                           </InfoLinksWrapper>
+                           <InfoLinksWrapper to='/'>
+                                <MDescr >Hierarchical Text-Conditional Image Generation with CLIP Latents</MDescr>
+                                <MTime >April 13, 2022 — Read blog post</MTime>
+                           </InfoLinksWrapper>
+                           <InfoLinksWrapper to='/'>
+                                <MDescr >Formal Mathematics Statement Curriculum Learning</MDescr>
+                                <MTime >February 3, 2022 — Read blog post</MTime>
+                           </InfoLinksWrapper>
+                            <StyledRouterBtn $nobg color='0,0,0' to="" after='▻' background=''>VIEW ALL PUBLICATIONS</StyledRouterBtn>
                         </InfoMain>
                     </InfoBlock>
                 </Container>
