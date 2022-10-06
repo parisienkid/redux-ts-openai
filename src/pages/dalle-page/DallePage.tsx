ff26e3cc59ef0f2a915efc4757f392026e77a694
Danya Boykov <danyaboickov@icloud.com>
2022-10-07 01:53:54 +0300

    Update DallePage.tsx

--- src/pages/dalle-page/DallePage.tsx
+++ src/pages/dalle-page/DallePage.tsx
@@ -9,7 +9,7 @@ import { RouterBtn } from '../../components/buttons/Btns';
 // styled components
 
 const IntroWrapper = styled.div`
-    padding: 180px 0;
+    padding: 25vh 0 140px 0;
     margin-top: -${({theme}) => theme.sizes.default.header.height}px;
     @media ${({theme}) => theme.media.extraLarge} {
         margin--top: ${({theme}) => theme.sizes.xl.header.height}px;
@@ -24,7 +24,9 @@ const IntroWrapper = styled.div`
         margin--top: ${({theme}) => theme.sizes.s.header.height}px;
     }
     h1 {
+        /* font-size: 17.3rem; */
         font-size: 17.3rem;
+        line-height: 17.3rem;
         text-align: center;
         color: white;
         word-wrap: break-word;
@@ -46,6 +48,62 @@ const IntroBtns = styled.div`
     position: absolute;
     right: 0;
     top: 10px;
+`
+
+const NavList = styled.ul`
+    display: flex;
+    list-style: none;
+    flex-direction: column;
+    margin-top: 80px;
+    padding-left: 53px;
+    li {
+
+    }
+`
+
+interface NavLinkProps {
+    after: string
+    before: string
+}
+
+const NavLink = styled.a<NavLinkProps>`
+    color: #fff;
+    display: block;
+    padding: 17px 5px;
+    position: relative;
+    font-family: ${({theme}) => theme.fonts.colfax};
+    font-weight: bold;
+    font-size: .85rem;
+    line-height: .85rem;
+    &:after {
+        content: '${props => props.after}';
+        display: block;
+        left: -25px;
+        position: absolute;
+        top: 50%;
+        margin-top: -1px;
+        transform: translateY(-50%);
+        font-family: IconsAI;
+        color: #fff;
+        font-size: .85rem;
+        line-height: .85rem;
+    }
+    &:before {
+        content: '';
+        display: block;
+        width: 5px;
+        background-color: ${props => props.before};
+        height: 100%;
+        position: absolute;
+        left:  -50px;
+        top: 0;
+    }
+    &:hover {
+        color: rgba(255,255,255,.6);
+        &:after {
+            color: rgba(255,255,255,.6);
+        }
+    }
 `
 
 //
@@ -70,6 +128,23 @@ const DallePage: FC = () => {
                 <Container>
                     <h1>DALL·E <span>2</span></h1>
                     <h2>DALL·E 2 is a new AI system that can create realistic images and art from a description in natural language.</h2>
+                    <NavList>
+                        <li>
+                            <NavLink before="#ff6" after='↗'>SIGN UP</NavLink>
+                        </li>
+                        <li>
+                            <NavLink before="#42ffff" after='↓'>EXPLORE</NavLink>
+                        </li>
+                        <li>
+                            <NavLink before="#51da4c" after='▶'>WATCH VIDEO</NavLink>
+                        </li>
+                        <li>
+                            <NavLink before="#ff6e3c" after='📄'>VIEW RESEARCH</NavLink>
+                        </li>
+                        <li>
+                            <NavLink before="#3c46ff" after=''>FOLLOW ON INSTAGRAM</NavLink>
+                        </li>
+                    </NavList>
                 </Container>
             </IntroWrapper>
         </>
